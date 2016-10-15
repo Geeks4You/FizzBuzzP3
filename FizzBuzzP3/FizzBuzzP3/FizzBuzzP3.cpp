2472bdd0f54604d04801bf12875f4e7e13d331f5
Marcia Allen <mallen57@email.phoenix.edu>
2016-10-15 16:29:59 -0400

    FizzBuzzP3
    Update

--- FizzBuzzP3/FizzBuzzP3/FizzBuzzP3.cpp
+++ FizzBuzzP3/FizzBuzzP3/FizzBuzzP3.cpp
@@ -38,6 +38,7 @@ CFizzbuzz::CFizzbuzz() {
 
 // getFizzBuzz function member of the CFizzbuzz class that will set values for the data members
 void CFizzbuzz::getFizzbuzz() {
+
 	int num;
 	string pname;
 	cout << "Enter the player's name: ";
@@ -51,24 +52,26 @@ void CFizzbuzz::getFizzbuzz() {
 }
 // putFizzBuzz function member that outputs the values of the data members.
 void CFizzbuzz::putFizzbuzz() {
-	cout << "Player Name: " << this->pname << endl;
+
+	cout << "Player Name: " << this->pname << "\n" << endl;
 
 	if ((this->itemNumber % 3 == 0) && (itemNumber % 5 == 0)) {
-		cout << "Item Number: " << itemNumber << " - FizzBuzz" << endl;
+		cout << "Item Number: " << itemNumber << " - FizzBuzz\n" << endl;
 	}
 	else if (itemNumber % 3 == 0) {
 
-		cout << "Item Number: " << itemNumber << " - Fizz" << endl;
+		cout << "Item Number: " << itemNumber << " - Fizz\n" << endl;
 	}
 	else if (itemNumber % 5 == 0) {
 
-		cout << "Item Number: " << itemNumber << " - Buzz" << endl;
+		cout << "Item Number: " << itemNumber << " - Buzz\n" << endl;
 	}
 }
 // getRecord() function so the program can detect when a zero item number is entered
 int CFizzbuzz::getRecord() {
+
 	if (this->itemNumber == 0) {
-		cout << "Game is over because the item number zero was entered" << endl;
+		cout << "\nGame is over because the item number zero was entered.\n" << endl;
 		return 0;
 	}
 	return itemNumber;
